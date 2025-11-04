479bef6bfd16a76346dced1208b270048beac288
CHERRYPOPUwU <erxiplay@gmail.com>
2025-11-03 19:30:00 -0500

    Fix: Eliminar puerto

--- app.py
+++ app.py
@@ -21,9 +21,6 @@ def create_app():
     app.config.from_object(Config)
     db.init_app(app)
 
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port) 
-
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
